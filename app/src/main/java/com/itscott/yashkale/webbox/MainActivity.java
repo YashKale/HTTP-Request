503967edcdf9dee5735f0eaa1216d071509d8755
yash kale <yashkale46@gmail.com>
2019-07-23 21:02:38 +0530

    HTTP Req

---
+++ app/src/main/java/com/itscott/yashkale/webbox/MainActivity.java
@@ -0,0 +1,162 @@
+package com.itscott.yashkale.webbox;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Iterator;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    EditText name,age,mob,city;
+    String nameX,ageX,mobX,cityX;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        name = findViewById(R.id.Name);
+        age = findViewById(R.id.Age);
+        mob = findViewById(R.id.Mob);
+        city = findViewById(R.id.City);
+
+
+
+    }
+
+    public void CallAsyncTask(View view) {
+
+        new SendPostRequest().execute();
+
+        String action;
+        Intent intent = new Intent(MainActivity.this,secondScreem.class);
+        startActivity(intent);
+
+    }
+
+    public class SendPostRequest extends AsyncTask<String, Void, String> {
+
+        protected void onPreExecute(){
+
+            nameX = name.getText().toString();
+            ageX = age.getText().toString();
+            mobX = mob.getText().toString();
+            cityX = city.getText().toString();
+
+        }
+
+        protected String doInBackground(String... arg0) {
+
+
+
+            try {
+                URL url = new URL("https://enr3ilnlxjfh.x.pipedream.net/"); // here is your URL path
+
+                JSONObject postDataParams = new JSONObject();
+
+                postDataParams.put("name", nameX);
+                postDataParams.put("age", ageX);
+                postDataParams.put("mobile",mobX);
+                postDataParams.put("city",cityX);
+
+                Log.e("params",postDataParams.toString());
+
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(15000 /* milliseconds */);
+                conn.setConnectTimeout(15000 /* milliseconds */);
+                conn.setRequestMethod("PUT");
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+
+                OutputStream os = conn.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(
+                        new OutputStreamWriter(os, "UTF-8"));
+                writer.write(getPostDataString(postDataParams));
+
+                writer.flush();
+                writer.close();
+                os.close();
+
+                int responseCode=conn.getResponseCode();
+
+                if (responseCode == HttpsURLConnection.HTTP_OK) {
+
+                    BufferedReader in=new BufferedReader(new
+                            InputStreamReader(
+                            conn.getInputStream()));
+
+                    StringBuffer sb = new StringBuffer("");
+                    String line="";
+
+                    while((line = in.readLine()) != null) {
+
+                        sb.append(line);
+                        break;
+                    }
+
+                    in.close();
+                    return sb.toString();
+
+                }
+                else {
+                    return new String("false : "+responseCode);
+                }
+            }
+            catch(Exception e){
+                return new String("Exception: " + e.getMessage());
+            }
+
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Toast.makeText(getApplicationContext(), result,
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public String getPostDataString(JSONObject params) throws Exception {
+
+        StringBuilder result = new StringBuilder();
+        boolean first = true;
+
+        Iterator<String> itr = params.keys();
+
+        while(itr.hasNext()){
+
+            String key= itr.next();
+            Object value = params.get(key);
+
+            if (first)
+                first = false;
+            else
+                result.append("&");
+
+            result.append(URLEncoder.encode(key, "UTF-8"));
+            result.append("=");
+            result.append(URLEncoder.encode(value.toString(), "UTF-8"));
+
+        }
+        return result.toString();
+    }
+}
--- app/src/main/java/com/itscott/yashkale/webbox/MethodBox.java
+++
@@ -1,7 +0,0 @@
-package com.itscott.yashkale.webbox;
-
-/**
- * Created by yash kale on 7/23/2019. 8:16 PM
- */
-public class MethodBox {
-}
